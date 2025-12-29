fcd4593d6554cff42f6d0078b218980f50a9e3ce
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2025-12-29 22:30:30 +0530

    header responsiveness fixed

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Header />
       <About />
-      {/* <Caption /> */}
+      <Caption />
       {/* <Experience /> */}
       {/* <Projects /> */}
       {/* <Contact /> */}
